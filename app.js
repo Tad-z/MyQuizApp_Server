e81f40be3ff6fd717807922070b2124d5c5ff877
Tad-z <david.akintade1000@gmail.com>
2023-09-13 12:16:41 +0100

    optimization

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
+const cron = require('node-cron');
+const axios = require('axios')
 const main = require("./models/db.js");
 const questionsRouter = require("./routes/questions.routes")
 const resultRouter = require("./routes/results.routes")
 require("dotenv").config();
 
 app.use(cors({
-    origin: ["http://localhost:3000","https://quiz-app-t6ai.onrender.com"]
+    origin: ["http://localhost:3000", "https://quiz-app-t6ai.onrender.com"]
 }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,8 +18,19 @@ app.use("/result", resultRouter);
 
 main()
     .then(() => {
-        app.listen(4000, () => {
+        app.listen(5000, () => {
             console.log("Server started...");
         })
         return console.log("DB connected...");
     }).catch(console.error);
+
+// Schedule the cron job to make a request every 10 minutes to keep the API alive
+cron.schedule('*/10 * * * *', async () => {
+    try {
+        // Make a GET request to a specific endpoint (e.g., /api/keep-alive)
+        const response = await axios.get(`https://https-quiz-app-server-onrender-com.onrender.com/questions`)
+        console.log('Response:', response.data);
+    } catch (error) {
+        console.error('Error:', error.message);
+    } console.error('Keep-alive request error:', error);
+});
