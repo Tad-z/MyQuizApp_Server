912fbcbb8adb8731e00aaaa61e74a21a4b7df2b6
Tad-z <david.akintade1000@gmail.com>
2023-11-07 18:47:32 +0100

    remove-cron

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
-const cron = require('node-cron');
-const axios = require('axios')
+// const cron = require('node-cron');
+// const axios = require('axios')
 const main = require("./models/db.js");
 const questionsRouter = require("./routes/questions.routes")
 const resultRouter = require("./routes/results.routes")
@@ -26,15 +26,15 @@ main()
     }).catch(console.error);
 
     // Schedule the cron job to make a request every 10 minutes to keep the API alive
-cron.schedule('*/14 * * * *', async () => {
-    try {
-        // Make a GET request to a specific endpoint (e.g., /api/keep-alive)
-        const response = await axios.get(`https://https-quiz-app-server-onrender-com.onrender.com/questions`)
-        console.log('Response:', response.data);
-    } catch (error) {
-        console.error('Error:', error.message);
-    } console.error('Keep-alive request error:', error);
-});
+// cron.schedule('*/14 * * * *', async () => {
+//     try {
+//         // Make a GET request to a specific endpoint (e.g., /api/keep-alive)
+//         const response = await axios.get(`https://https-quiz-app-server-onrender-com.onrender.com/questions`)
+//         console.log('Response:', response.data);
+//     } catch (error) {
+//         console.error('Error:', error.message);
+//     } console.error('Keep-alive request error:', error);
+// });
 
 module.exports = app;
 
